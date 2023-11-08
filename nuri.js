174f07dab3425418f20b682ed90e8e13b2004767
Nuricantikk <148602051+Nuricantikk@users.noreply.github.com>
2023-11-08 16:55:52 +0700

    Update and rename sahar.js to nuri.js

--- nuri.js
+++ nuri.js
@@ -2,11 +2,11 @@ function Login() {
     var done = 0;
     var username = document.login.username.value;
     var password = document.login.password.value;
-    if (username == "SaharGanteng" && password =="SaharGanteng"){
+    if (username == "Nuri" && password =="Nuri"){
             if(confirm("Apakah Anda Ingin Login?") == true){
-                window.location.href = "https://github.com/vinsmkod"
+                window.location.href = "https://github.com/Nuricantikk"
             } else {
-                alert("Login dibatalkan karena anda bukan jawa!");
+                alert("Login dibatalkan karena anda salah!");
             }
             
     } else{
